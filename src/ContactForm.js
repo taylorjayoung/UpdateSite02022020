1303743b1dcfd74e4f02fece8786a7b3514bb4f5
SlickJacket <novenarii@gmail.com>
2019-10-15 15:48:17 -0400

    first commit

---
+++ src/ContactForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
+import './ContactForm.css'
+
+class ContactForm extends Component {
+    state = {
+        from_name: "",
+        from_email: "",
+        project_request: ""
+
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+    
+        let templateParams = {
+        from_name: this.state.from_name,
+        from_email: this.state.from_email,
+        project_request: this.state.project_request
+    };
+    
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATEID
+    const userId = process.env.REACT_APP_EMAILJS_USERID
+    
+    emailjs.send( 'gmail', templateId, templateParams,  userId )
+        .then((response) => {
+            this.setState({from_email: "", from_name: "", project_request: ""})
+            alert("Message Sent!")
+            console.log('SUCCESS!', response.status, response.text);
+        }, (err) => {
+            alert("Something Went Wrong 😭")
+            console.log('FAILED...', err);
+        });
+    }
+
+
+
+    render() { 
+        return ( 
+
+            <div>
+                <form className="form-style-4" onSubmit={this.handleSubmit} >
+                <h1>Contact Me</h1>
+                    <input placeholder="Name" className="text-input" id="from_name" name="from_name" onChange={this.handleChange}  
+                    required value={this.state.from_name} />
+
+                    <input placeholder="Email" className="text-input" id="from_email"  name="from_email" onChange={this.handleChange}  
+                        required value={this.state.from_email} />
+
+                    <textarea className="text-input" id="project_request" name="project_request" onChange={this.handleChange}  
+                    required value={this.state.project_request} />
+                    
+                    <input type="submit" value="Submit" className="btn btn--submit"  />
+
+                    
+                </form>
+            </div>
+            );
+    }
+}
+
+export default ContactForm;
