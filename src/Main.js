d217c5eec91531cf5893bc478b45720a036a4435
SlickJacket <novenarii@gmail.com>
2020-02-02 11:26:16 -0500

    conact page mobilized

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import "./App.css";
 import Contact from "./ContactForm";
 import Home from "./Home";
